43893218940de45f2a4a76bac0ed523ac29762dd
Trevor Richard <trmidpoint@gmail.com>
2023-09-26 16:51:07 +0000

    update schema and hash ID function

--- src/helpers/common.ts
+++ src/helpers/common.ts
@@ -1,15 +1,15 @@
-import { BigInt, ethereum, crypto, ByteArray, Bytes } from '@graphprotocol/graph-ts';
+import { BigInt, ethereum, crypto, Bytes } from '@graphprotocol/graph-ts';
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
-
 export const ZERO = BigInt.fromI32(0);
 export const ONE = BigInt.fromI32(1);
 
-export const generateUniqueId = (...keys: Bytes[]): Bytes => {
+export const generateHashId = (...keys: Bytes[]): Bytes => {
   let concatenated = new Bytes(0);
   for (let i = 0; i < keys.length; i++) {
     concatenated = concatenated.concat(keys[i]);
   }
   return crypto.keccak256(concatenated);
-}
-export const generateUniqueLogId = (event: ethereum.Event): Bytes => generateUniqueId(event.transaction.hash, Bytes.fromBigInt(event.logIndex));
+};
+
+export const generateUniqueLogId = (event: ethereum.Event): Bytes => generateHashId(event.transaction.hash, Bytes.fromBigInt(event.logIndex));
